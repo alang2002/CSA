0163a4a87437c1e129a071782da8b119b6f5442c
Aaron Lang <blackthornlm@gmail.com>
2018-02-28 13:51:25 -0600

    Finished 1.2.2

--- MediaLib/ArrayMediaLib.java
+++ MediaLib/ArrayMediaLib.java
@@ -14,6 +14,7 @@ public class ArrayMediaLib
                 new Song("Party Rock Anthem"), new Song("I Gotta Feeling"), new Song("Macarena (Bayside Boys Mix)"), new Song("Physical"), 
                 new Song("You Light Up My Life"), new Song("Hey Jude")};
         int runningTotal = 0;
+        int counter = 0;
         for (int i = 0; i < sharingFriends.length; i++) {
             System.out.println(sharingFriends[i]);
         }
@@ -22,12 +23,36 @@ public class ArrayMediaLib
         }
         System.out.println(runningTotal / daysBtwnPurchase.length);
         System.out.println();
+        // Setting the price then displaying the song along with it
         for (Song s: topTenSongs)
         {
-            System.out.println(s.getTitle());
+            s.setPrice(1.00);
+            counter += 1;
+            // Every third song is discounted by 99 cents
+            if (counter == 3) {
+                s.setPrice(.01);
+                counter = 0;
+            }
+            System.out.println(s.getTitle() + " " + s.getPrice());
         }
-        for (Song changeSong : topTenSongs) {
-            changeSong.setPrice(1.00);
+        System.out.println();
+        // Showing the first five songs
+        for (int i = 0; i < topTenSongs.length && i < 5; i++) {
+            System.out.println(topTenSongs[i].getTitle() + " " + topTenSongs[i].getPrice());
+        }
+        System.out.println();
+        // Showing every other song
+        for (int i = 0; i < topTenSongs.length; i += 2) {
+            System.out.println(topTenSongs[i].getTitle() + " " + topTenSongs[i].getPrice());
+        }
+        System.out.println();
+        // Using for each loop and if statement as well
+        for (Song song : topTenSongs) {
+            counter += 1;
+            if (counter == 2) {
+                System.out.println(song.getTitle() + " " + song.getPrice());
+                counter = 0;
+            }
         }
 
         /* change the array
